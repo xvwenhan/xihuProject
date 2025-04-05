adee7d438215ea8d839ebc6d245139e3b1c5e5b8
sleep-sleep-yyy <1051095699@qq.com>
2025-04-06 00:02:49 +0800

    feat 添加订阅相关接口

--- xihu_backend/UserService/Data/UserDbContext.cs
+++ xihu_backend/UserService/Data/UserDbContext.cs
@@ -104,6 +104,101 @@ namespace UserService.Data
                 entity.HasIndex(e => e.SendTime);
                 entity.HasIndex(e => new { e.Email, e.Purpose });
             });
+
+            modelBuilder.Entity<Conference>(entity =>
+            {
+                entity.ToTable("conference");  // 映射数据库表名
+
+                entity.HasKey(e => e.ConferenceId);
+
+                entity.Property(e => e.ConferenceId)
+                    .HasColumnName("conference_id");
+
+                entity.Property(e => e.ConferenceName)
+                    .HasColumnName("conference_name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.StartTime)
+                    .HasColumnName("start_time")
+                    .HasColumnType("time");
+
+                entity.Property(e => e.EndTime)
+                    .HasColumnName("end_time")
+                    .HasColumnType("time");
+
+                entity.Property(e => e.Type)
+                    .HasColumnName("type")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.IsOnlyOffline)
+                    .HasColumnName("is_only_offline")
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.Location)
+                    .HasColumnName("location")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.OfflineNum)  // 映射 offline_num 字段
+                    .HasColumnName("offline_num")
+                    .HasDefaultValue(0)  // 默认值 0
+                    .IsRequired();  // 不能为空
+
+                entity.Property(e => e.SubscribeNum)  // 映射 offline_num 字段
+                    .HasColumnName("subscribe_num")
+                    .HasDefaultValue(0)  // 默认值 0
+                    .IsRequired();  // 不能为空
+
+                entity.Property(e => e.Url)
+                    .HasColumnName("url")
+                    .HasMaxLength(255);
+            });
+            modelBuilder.Entity<Subscribe>(entity =>
+            {
+                entity.ToTable("subscribe");  // 映射数据库表名
+
+                // 设置联合主键
+                entity.HasKey(uc => new { uc.UserId, uc.ConferenceId });
+
+                // 配置 UserId 的数据库字段映射
+                entity.Property(e => e.UserId)
+                      .HasColumnName("user_id");
+
+                // 配置 ConferenceId 的数据库字段映射
+                entity.Property(e => e.ConferenceId)
+                      .HasColumnName("conference_id");
+
+                // 配置外键约束
+                entity.HasOne(uc => uc.User)
+                    .WithMany()  // 一个用户可以有多个会议订阅
+                    .HasForeignKey(uc => uc.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);  // 级联删除
+
+                entity.HasOne(uc => uc.Conference)
+                    .WithMany()  // 一个会议可以有多个订阅
+                    .HasForeignKey(uc => uc.ConferenceId)
+                    .OnDelete(DeleteBehavior.Cascade);  // 级联删除
+            });
+            modelBuilder.Entity<PushSubscriptions>(entity =>
+            {
+                entity.ToTable("PushSubscriptions");
+                entity.HasKey(e => e.SubscriptionId);
+                entity.Property(e => e.Endpoint).HasMaxLength(500);
+                entity.Property(e => e.P256dhKey).HasMaxLength(500);
+                entity.Property(e => e.AuthKey).HasMaxLength(500);
+                entity.Property(e => e.UserId);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP(6)");
+
+                // 配置外键约束
+                entity.HasOne(uc => uc.User)
+                    .WithMany()  // 一个用户可以有多个浏览器
+                    .HasForeignKey(uc => uc.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);  // 级联删除
+
+            });
         }
     }
 }
---
+++ xihu_backend/UserService/Models/Subscribe.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserService.Models
+{
+    [Table("subscribe")] // 对应数据库表名
+    public class Subscribe
+    {
+        [Key]
+        [Column("user_id")]  // 映射数据库字段
+        public int UserId { get; set; }
+
+        [Key]
+        [Column("conference_id")]  // 映射数据库字段
+        public int ConferenceId { get; set; }
+
+        // 导航属性
+        public User User { get; set; }
+        public Conference Conference { get; set; }
+    }
+}
