46d69bfcc1dff0e457fcf75fc0c06c7958650b27
sleep-sleep-yyy <1051095699@qq.com>
2025-05-09 23:11:43 +0800

    可以给智能体传userid了~

--- xihu_backend/ChatService/Services/AgentService.cs
+++ xihu_backend/ChatService/Services/AgentService.cs
@@ -56,8 +56,11 @@ namespace ChatService.Services
                 {
                     id = _appSettings.ModelId_New,
                     sid = sid,
-                    inputs = request.input ,
-
+                    inputs = new Dictionary<string, string>
+                        {
+                            { "input", request.input },
+                            { "userId", userId }
+                        }
                  };
 
                 SetHeaders();
@@ -95,7 +98,35 @@ namespace ChatService.Services
             return ApiResponse.Success<ChatResult>(response, "未存入缓存");
 
         }
-        
+        // public async Task<ApiResponse<ChatResult>> ExecuteAgentAsync(InputRequest request, string userId)
+        // {
+        //     var sid = Guid.NewGuid().ToString();
+        //     var passinreq = new AgentExecuteRequest
+        //     {
+        //         id = _appSettings.ModelId,
+        //         sid = sid,
+        //         input = request.input
+        //     };
+        //     SetHeaders();
+
+        //     var jsonContent = JsonSerializer.Serialize(passinreq);
+        //     var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+        //     //向模型发送问题
+        //     var response = await client.PostAsync("https://www.das-ai.com/open/api/v2/agent/execute", content);
+        //     var temp = await response.Content.ReadAsStringAsync();
+
+        //     var apiResponse = JsonSerializer.Deserialize<ApiJResponse>(temp);
+
+        //     StoreToDBAsync(apiResponse, userId);
+        //     var answer = new ChatResult
+        //     {
+        //         session = apiResponse.data.session,
+        //         time = DateTime.Now,
+        //     };
+        //     //todo 还要改成封装版本
+        //     return ApiResponse.Success(answer);
+        // }
+
         public async Task<ApiResponse<PagedResult>> GetChatLogs(string userId,int page,int pageSize)
         {
             var result = await _mongoDBContext.GetChatHistoryPagedAsync(userId, page, pageSize);
@@ -115,6 +146,32 @@ namespace ChatService.Services
             client.DefaultRequestHeaders.Add("sign", sign);
         }
 
+        private async Task<bool> StoreToDBAsync(ChatResult res,string uid)
+        {
+            // 创建新的 JSON 对象，包含 data、tid 和当前时间
+            var newJsonObject = new
+            {
+                ChatResult = res,
+                userId = uid
+            };
+            var bsonDocument = BsonDocument.Parse(JsonSerializer.Serialize(newJsonObject));
+
+            // 插入到 MongoDB
+            await _mongoDBContext.InsertDocumentAsync("chat_logs", bsonDocument);
+            Console.WriteLine("数据已成功存储到 MongoDB！");
+            return true;
+        }
+
+        private string GenerateSign(string key, string secret)
+        {
+            long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            string data = $"{timestamp}\n{secret}\n{key}";
+            using (var hmacsha256 = new HMACSHA256(Encoding.UTF8.GetBytes(secret)))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(Encoding.UTF8.GetBytes(data));
+                return $"{timestamp}{Convert.ToBase64String(hashmessage)}";
+            }
+        }
 
 
     }
